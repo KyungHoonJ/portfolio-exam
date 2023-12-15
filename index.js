26828c1f76ee3b40a71fe36fd57bead6c2f9eebd
정경훈 <jeong-gyeonghun@jeong-gyeonghun-ui-MacBookPro.local>
2023-12-15 14:52:20 +0900

    feat: add modal

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-document.getElementById("modal").onscroll = (e) => {
+const modalElem = document.getElementById("modal");
+
+modalElem.onwheel = (e) => {
   e.stopPropagation();
   e.preventDefault();
 };
-document.getElementById("modal").onwheel = (e) => {
-  e.stopPropagation();
-  e.preventDefault();
+
+modalElem.onclick = document.getElementById("close-btn").onclick = () => {
+  modalElem.style.display = "";
 };
+
+document.getElementById("modal-box").onclick = (e) => {
+  e.stopPropagation();
+};
+
+const projectArr = {
+  firstProject: { details: "어쩌구 저쩌구~" },
+  secondProject: { details: "어쩌구2 저쩌구~2" },
+};
+
+[...document.getElementsByClassName("project-info")].forEach((i) => {
+  i.onclick = (e) => {
+    modalElem.style.display = "flex";
+    modalElem.getElementsByClassName("details")[0].innerHTML =
+      projectArr[e.target.dataset.name].details;
+  };
+});

9055cbc7b0e720b2cf7dd9ecff04acea4974e9d9
정경훈 <jeong-gyeonghun@jeong-gyeonghun-ui-MacBookPro.local>
2023-12-15 14:29:03 +0900

    testing

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("modal").onscroll = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+};
+document.getElementById("modal").onwheel = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+};
